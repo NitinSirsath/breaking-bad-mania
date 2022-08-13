89de01c70ef28fafc14d5b8e63100de7cb358a26
lostNIII3 <73011138+NitinSirsath@users.noreply.github.com>
2022-08-13 15:46:34 +0530

    updated about page information

--- pages/about.jsx
+++ pages/about.jsx
@@ -7,8 +7,8 @@ import github from '../public/github.png'
 const about = () => {
   return (
     <div className={style.container}>
-        <h1>Summary</h1>
-        <p>Hello Guys, I build this application as a assignment  project  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor soluta, iusto sequi adipisci laudantium fugit quis, et veritatis est magni,  delectus officia facere incidunt hic porro tempora enim asperiores! Vero. Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae molestiae sapiente error cumque, quidem dicta sunt animi quod deleniti ducimus distinctio suscipit doloremque minus ex at? Magnam laboriosam nemo optio?</p>
+        <h1>APPLICATION EXPERIENCE</h1>
+        <p>Hello guys, I build this application under the induction program of AntStack. Application is created in nextJS. NextJs is an open-source web development framework built on top of Node.js enabling React-based web applications functionalities such as server-side rendering and generating static websites. In this project I have encounter some of basic functionality that nextJS offers like lazy loading , file based routing , dynamic nested routing and my mentor Judith Joseph told to use new styling library so I have used SASS for styling the apllication. SASS is extension of CSS that enables you to use things like variables, nested rules, inline imports and more.</p>
         <a className={style.github} rel='noreferrer' target='_blank' href='https://github.com/NitinSirsath/breaking-bad-mania'><Image src={github} alt='github' height={50} width={50}/></a>
     </div>
   )
